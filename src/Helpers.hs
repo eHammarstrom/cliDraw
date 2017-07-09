ff36553a80b98f47d79994fc84b5c4d85d9178b5
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2017-07-09 17:32:18 +0200

    left, top borders

--- src/Helpers.hs
+++ src/Helpers.hs
@@ -26,9 +26,12 @@ move :: Point -> IO ()
 move (x, y) = putStr (constructEscape (y : [x]) "H")
 
 moveUp :: Point -> IO Point
-moveUp (x, y) = do
-  putStr (constructEscape [1] "A")
-  return (x, y-1)
+moveUp (x, y) =
+  if y-1 > 0 then do
+    putStr (constructEscape [1] "A")
+    return (x, y-1)
+  else
+    return (x, y)
 
 moveDown :: Point -> IO Point
 moveDown (x, y) = do
@@ -36,9 +39,12 @@ moveDown (x, y) = do
   return (x, y+1)
 
 moveLeft :: Point -> IO Point
-moveLeft (x, y) = do
-  putStr (constructEscape [1] "D")
-  return (x-1, y)
+moveLeft (x, y) =
+  if x-1 > 0 then do
+    putStr (constructEscape [1] "D")
+    return (x-1, y)
+  else
+    return (x, y)
 
 moveRight :: Point -> IO Point
 moveRight (x, y) = do
