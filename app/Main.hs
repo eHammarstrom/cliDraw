ac3d43b7fa9d260f2c22fe3d338ca8b86704b64c
Emil Hammarström <emil.a.hammarstrom@gmail.com>
2017-07-10 11:38:41 +0000

    Refactored Main and Helpers

--- app/Main.hs
+++ app/Main.hs
@@ -1,66 +1,56 @@
 module Main where
 
-import Helpers
-import Data.List
 import Data.Char
+import Data.List
+import Helpers
 import System.IO
 
-data DrawData = DrawData {
-  pos   :: Point,
-  ps    :: [Point],
-  color :: Color
-} deriving (Show)
+data DrawData = DrawData
+  { pos :: Point
+  , ps :: [Point]
+  , color :: Color
+  } deriving (Show)
 
 main :: IO ()
 main = do
   startPos <- getTermCenter
   let color = (37, 40)
-
   setColor color
   clear
   move startPos
-
-  mainLoop DrawData { pos = startPos, ps = [], color = color }
+  mainLoop DrawData {pos = startPos, ps = [], color = color}
 
 mainLoop :: DrawData -> IO ()
 mainLoop dd = do
   hSetEcho stdout False
   action <- getChar
   dd' <- actionHandler [action] dd
-
-  let putStrOnPoint'  = putStrOnPoint (pos dd')
-  let ps'   = zip (ps dd') (replicate (length (ps dd')) "█")
+  let putStrOnPoint' = putStrOnPoint (pos dd')
+  let ps' = zip (ps dd') (replicate (length (ps dd')) "█")
   clear
   mapM_ putStrOnPoint' ps'
-
   mainLoop dd'
 
-up    = ["k", "\ESC[A"]
-down  = ["j", "\ESC[B"]
-left  = ["h", "\ESC[D"]
+up = ["k", "\ESC[A"]
+
+down = ["j", "\ESC[B"]
+
+left = ["h", "\ESC[D"]
+
 right = ["l", "\ESC[C"]
 
 actionHandler :: String -> DrawData -> IO DrawData
 actionHandler c dd
-  | c == "\n" = if pos' `elem` ps'
-                   then return dd { ps = delete pos' ps' }
-                   else return dd { ps = pos' : ps' }
-  | c == "c" = do
-    sequence_ (clear : [move pos'])
-    return dd { ps = [] }
-  | c `elem` up = do
-    p <- moveUp pos'
-    return dd { pos = p }
-  | c `elem` down = do
-    p <- moveDown pos'
-    return dd { pos = p }
-  | c `elem` left = do
-    p <- moveLeft pos'
-    return dd { pos = p }
-  | c `elem` right = do
-    p <- moveRight pos'
-    return dd { pos = p }
+  | c == "\n" =
+    if pos' `elem` ps'
+      then return dd {ps = delete pos' ps'}
+      else return dd {ps = pos' : ps'}
+  | c == "c" = return dd {ps = []}
+  | c `elem` up = moveUp pos' >>= \p -> return dd {pos = p}
+  | c `elem` down = moveDown pos' >>= \p -> return dd {pos = p}
+  | c `elem` left = moveLeft pos' >>= \p -> return dd {pos = p}
+  | c `elem` right = moveRight pos' >>= \p -> return dd {pos = p}
   | otherwise = return dd
   where
-    pos'  = pos dd
-    ps'   = ps dd
+    pos' = pos dd
+    ps' = ps dd
--- src/Helpers.hs
+++ src/Helpers.hs
@@ -1,15 +1,15 @@
-module Helpers(
-  Point,
-  Color,
-  putStrOnPoint,
-  move,
-  moveUp,
-  moveDown,
-  moveLeft,
-  moveRight,
-  setColor,
-  clear,
-  getTermCenter
+module Helpers
+  ( Point
+  , Color
+  , putStrOnPoint
+  , move
+  , moveUp
+  , moveDown
+  , moveLeft
+  , moveRight
+  , setColor
+  , clear
+  , getTermCenter
   ) where
 
 import Data.Char
@@ -17,6 +17,7 @@ import Data.List
 import System.Console.Terminal.Size
 
 type Point = (Int, Int) -- x  y
+
 type Color = (Int, Int) -- fg bg
 
 putStrOnPoint :: Point -> (Point, String) -> IO ()
@@ -30,29 +31,29 @@ move (x, y) = putStr (constructEscape (y : [x]) "H")
 
 moveUp :: Point -> IO Point
 moveUp (x, y) =
-  if y-1 > 0 then do
-    putStr (constructEscape [1] "A")
-    return (x, y-1)
-  else
-    return (x, y)
+  if y - 1 > 0
+    then do
+      putStr (constructEscape [1] "A")
+      return (x, y - 1)
+    else return (x, y)
 
 moveDown :: Point -> IO Point
 moveDown (x, y) = do
   putStr (constructEscape [1] "B")
-  return (x, y+1)
+  return (x, y + 1)
 
 moveLeft :: Point -> IO Point
 moveLeft (x, y) =
-  if x-1 > 0 then do
-    putStr (constructEscape [1] "D")
-    return (x-1, y)
-  else
-    return (x, y)
+  if x - 1 > 0
+    then do
+      putStr (constructEscape [1] "D")
+      return (x - 1, y)
+    else return (x, y)
 
 moveRight :: Point -> IO Point
 moveRight (x, y) = do
   putStr (constructEscape [1] "C")
-  return (x+1, y)
+  return (x + 1, y)
 
 setColor :: Color -> IO ()
 setColor (fg, bg) = putStr (constructEscape (fg : [bg]) "m")
@@ -64,10 +65,9 @@ getTermCenter :: IO Point
 getTermCenter = do
   s <- size
   case s of
-       Just (Window h w) -> return (w `div` 2, h `div` 2)
-       _                 -> return (0, 0)
+    Just (Window h w) -> return (w `div` 2, h `div` 2)
+    _ -> return (0, 0)
 
 {- private -}
-
 constructEscape :: [Int] -> String -> String
 constructEscape args code = "\ESC[" ++ intercalate ";" (map show args) ++ code
